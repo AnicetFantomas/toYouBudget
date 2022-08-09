017feddbaa9751b0b49546cd5c991565cc238aa9
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-09 13:06:08 +0200

    Update models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
-  has_many :categories_transactions
-  validates :amount, presence:true
+  has_many :categories_transactions, dependent: :delete_all
+  validates :amount, presence:true, through: :categories_transactions
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :transactions, dependent: :destroy
+  has_many :transactions, dependent: :destroy
+  validates :full_name, presence: true
+  validates :email, presence: true
 end
