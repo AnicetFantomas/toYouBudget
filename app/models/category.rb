c1319f698af9f36782de876605be508dc20333b4
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-09 12:57:23 +0200

    Update models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
+  has_many :categories_transactions
+  has_many :tansactions
+  validates :name, presence: true
+
+  def amount 
+    0
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
+  has_many :categories_transactions
+  validates :amount, presence:true
+  validates :name, presence: true
 end
