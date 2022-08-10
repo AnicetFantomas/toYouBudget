4a382f33cd699dd79f6f12a5a5233a01ba174c80
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-10 08:31:37 +0200

    Add transaction model test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   has_many :categories_transactions, dependent: :delete_all
-  validates :amount, presence:true, through: :categories_transactions
+  has_many :categories, through: :categories_transactions
+  validates :amount, presence:true
   validates :name, presence: true
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Transactions1, type: :model do
+RSpec.describe Transaction, type: :model do
   context 'Testing validations on a single subject' do
     before(:all) do
       @user = User.create(full_name: 'Anicet Shemeki', password: '123456', email: 'anicetshemeki@gmail.com')
     end
-    subject { Transactions.new(name: 'Greengrocery', amount: 20, user_id: @user) }
+    subject { Transaction.new(name: 'Greengrocery', amount: 20, user_id: @user) }
     before { subject.save }
 
     it 'name must not be blank' do
